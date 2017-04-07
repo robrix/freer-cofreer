6457773f8ce355ff4f8009107d0b5773bff26853
Rob Rix <rob.rix@me.com>
2017-04-07 17:21:26 -0400

    Test that the Show instance shows complete programs.

--- test/Control/Monad/Free/Freer/Spec.hs
+++ test/Control/Monad/Free/Freer/Spec.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE GADTs #-}
 module Control.Monad.Free.Freer.Spec where
 
 import Control.Monad.Free.Freer
+import Data.Functor.Classes
 import Test.Hspec
 import Test.Hspec.LeanCheck
 
@@ -13,3 +15,28 @@ spec = do
 
       prop "is commutative" $
         \ a b -> a == b `shouldBe` b == (a :: Freer Maybe Int)
+
+    describe "Show" $ do
+      it "shows complete programs" $
+        show (do
+          set 0
+          test 0
+          set 1
+          test 1
+          pure ()) `shouldBe` "Set 0\nTest 0\nSet 1\nTest 1\nReturn ()"
+
+
+data Instr a where
+  Set :: Int -> Instr ()
+  Test :: Int -> Instr Bool
+
+set :: Int -> Freer Instr ()
+set i = Set i `Then` return
+
+test :: Int -> Freer Instr Bool
+test i = Test i `Then` return
+
+instance Show1 Instr where
+  liftShowsPrec sp _ d i = case i of
+    Set i -> showsUnaryWith showsPrec "Set" d i . showChar '\n' . sp d ()
+    Test i -> showsUnaryWith showsPrec "Test" d i . showChar '\n' . sp d False
