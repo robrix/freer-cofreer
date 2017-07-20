720961d61650bfdcd06fda414d70e95dd28fa13a
Rob Rix <rob.rix@me.com>
2017-07-20 18:39:03 -0400

    Move FreerF into Control.Monad.Free.Freer.

--- src/Control/Monad/Free/Freer.hs
+++ src/Control/Monad/Free/Freer.hs
@@ -12,7 +12,6 @@ module Control.Monad.Free.Freer
 
 import Control.Monad ((>=>))
 import Control.Monad.Free.Class hiding (liftF)
-import qualified Control.Monad.Trans.Free.Freer as T
 import Data.Bifunctor
 import Data.Functor.Classes
 import Data.Functor.Foldable
@@ -24,32 +23,49 @@ data Freer f a where
 
 infixl 1 `Then`
 
+liftF :: f a -> Freer f a
+liftF action = action `Then` return
+{-# INLINE liftF #-}
+
+
+data FreerF f a b where
+  ReturnF :: a -> FreerF f a b
+  ThenF :: f x -> (x -> b) -> FreerF f a b
+
+liftFreerF :: f b -> FreerF f a b
+liftFreerF action = action `ThenF` id
+{-# INLINE liftFreerF #-}
+
+
+hoistFreerF :: (forall a. f a -> g a) -> FreerF f b c -> FreerF g b c
+hoistFreerF f r = case r of
+  ReturnF a -> ReturnF a
+  ThenF r t -> ThenF (f r) t
+
+
 iter :: Functor f => (f a -> a) -> Freer f a -> a
 iter algebra = iterFreer ((algebra .) . flip fmap)
 
 iterA :: (Functor f, Applicative m) => (f (m a) -> m a) -> Freer f a -> m a
 iterA algebra = cata $ \ r -> case r of
-  T.Return a -> pure a
-  T.Then r t -> algebra (t <$> r)
+  ReturnF a -> pure a
+  ThenF r t -> algebra (t <$> r)
 
 iterFreer :: (forall x. f x -> (x -> a) -> a) -> Freer f a -> a
 iterFreer algebra = cata $ \ r -> case r of
-  T.Return a -> a
-  T.Then r t -> algebra r t
+  ReturnF a -> a
+  ThenF r t -> algebra r t
 
 iterFreerA :: Applicative m => (forall x. f x -> (x -> m a) -> m a) -> Freer f a -> m a
 iterFreerA algebra = cata $ \ r -> case r of
-  T.Return a -> pure a
-  T.Then r t -> algebra r t
+  ReturnF a -> pure a
+  ThenF r t -> algebra r t
 
 hoistFreer :: (forall a. f a -> g a) -> Freer f b -> Freer g b
 hoistFreer f = go
   where go r = case r of
           Return a -> Return a
           Then r t -> Then (f r) (go . t)
-
-liftF :: f a -> Freer f a
-liftF = flip Then pure
 
 
 -- Instances
@@ -95,19 +111,6 @@ instance Traversable f => Traversable (Freer f) where
   {-# INLINE traverse #-}
 
 
-type instance Base (Freer f a) = T.FreerF f a
-
-instance Recursive (Freer f a) where
-  project (Return a) = T.Return a
-  project (Then r t) = T.Then r t
-  {-# INLINE project #-}
-
-instance Corecursive (Freer f a) where
-  embed (T.Return a) = Return a
-  embed (T.Then r t) = Then r t
-  {-# INLINE embed #-}
-
-
 instance Show1 f => Show1 (Freer f) where
   liftShowsPrec sp sl = go
     where go d r = case r of
@@ -132,3 +135,76 @@ instance Listable1 f => Listable1 (Freer f) where
 
 instance (Listable a, Listable1 f) => Listable (Freer f a) where
   tiers = liftTiers tiers
+
+
+instance Functor (FreerF f a) where
+  fmap _ (ReturnF a) = ReturnF a
+  fmap f (ThenF r g) = ThenF r (f . g)
+  {-# INLINE fmap #-}
+
+instance Bifunctor (FreerF f) where
+  bimap f g r = case r of
+    ReturnF a -> ReturnF (f a)
+    ThenF r t -> ThenF r (g . t)
+  {-# INLINE bimap #-}
+
+
+instance Foldable f => Foldable (FreerF f a) where
+  foldMap f g = case g of
+    ReturnF _ -> mempty
+    ThenF r t -> foldMap (f . t) r
+  {-# INLINE foldMap #-}
+
+instance Traversable f => Traversable (FreerF f a) where
+  traverse f g = case g of
+    ReturnF a -> pure (ReturnF a)
+    ThenF r t -> liftFreerF <$> traverse (f . t) r
+  {-# INLINE traverse #-}
+
+
+instance Eq1 f => Eq2 (FreerF f) where
+  liftEq2 eqA eqB f1 f2 = case (f1, f2) of
+    (ReturnF a1, ReturnF a2) -> eqA a1 a2
+    (ThenF r1 t1, ThenF r2 t2) -> liftEq (\ x1 x2 -> eqB (t1 x1) (t2 x2)) r1 r2
+    _ -> False
+
+instance (Eq1 f, Eq a) => Eq1 (FreerF f a) where
+  liftEq = liftEq2 (==)
+
+instance (Eq1 f, Eq a, Eq b) => Eq (FreerF f a b) where
+  (==) = liftEq (==)
+
+
+instance Show1 f => Show2 (FreerF f) where
+  liftShowsPrec2 sp1 _ sp2 sa2 d f = case f of
+    ReturnF a -> showsUnaryWith sp1 "Return" d a
+    ThenF r t -> showsBinaryWith (liftShowsPrec (\ i -> sp2 i . t) (sa2 . fmap t)) (const showString) "Free" d r "_"
+
+instance (Show1 f, Show a) => Show1 (FreerF f a) where
+  liftShowsPrec = liftShowsPrec2 showsPrec showList
+
+instance (Show1 f, Show a, Show b) => Show (FreerF f a b) where
+  showsPrec = liftShowsPrec showsPrec showList
+
+
+instance Listable1 f => Listable2 (FreerF f) where
+  liftTiers2 t1 t2 = liftCons1 t1 ReturnF \/ liftCons1 (liftTiers t2) liftFreerF
+
+instance (Listable a, Listable1 f) => Listable1 (FreerF f a) where
+  liftTiers = liftTiers2 tiers
+
+instance (Listable a, Listable b, Listable1 f) => Listable (FreerF f a b) where
+  tiers = liftTiers tiers
+
+
+type instance Base (Freer f a) = FreerF f a
+
+instance Recursive (Freer f a) where
+  project (Return a) = ReturnF a
+  project (Then r t) = ThenF r t
+  {-# INLINE project #-}
+
+instance Corecursive (Freer f a) where
+  embed (ReturnF a) = Return a
+  embed (ThenF r t) = Then r t
+  {-# INLINE embed #-}
